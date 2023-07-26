c3c21ac7ba06d646ae54e78e6d1c4ac061ee1ef6
Yū Kobayashi <kb10uy@kb10uy.org>
2023-07-26 13:08:01 +0900

    Add monaxia_data::id test

--- backend/monaxia-data/src/id.rs
+++ backend/monaxia-data/src/id.rs
@@ -34,10 +34,10 @@ pub fn to_order58(uuid: Uuid) -> String {
     buffer[(cursor + 1)..].iter().collect()
 }
 
-pub fn from_order58(t36: &str) -> Result<Uuid, Order58Error> {
+pub fn from_order58(o58: &str) -> Result<Uuid, Order58Error> {
     let mut value = 0u128;
-    for c in t36.chars() {
-        value *= 36;
+    for c in o58.chars() {
+        value *= 58;
         value += ORDER58_CHARS
             .binary_search(&c)
             .map_err(|_| Order58Error::InvalidDigit)? as u128;
@@ -56,5 +56,43 @@ pub fn cmp_mstdn_id(lhs: &str, rhs: &str) -> Ordering {
         // if equal length, compare by lexicial order.
         Ordering::Equal => lhs.cmp(rhs),
         otherwise => otherwise,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{cmp_mstdn_id, from_order58, now_order58, to_order58};
+
+    use std::{cmp::Ordering, thread::sleep, time::Duration};
+
+    use uuid::Uuid;
+
+    #[test]
+    fn order58_works() {
+        let _ = now_order58();
+
+        let base_uuid = Uuid::now_v7();
+        let encoded = to_order58(base_uuid);
+        let decoded = from_order58(&encoded);
+
+        assert!(decoded.is_ok());
+        assert_eq!(decoded.unwrap(), base_uuid);
+    }
+
+    #[test]
+    fn mstdn_cmp_works() {
+        assert_eq!(cmp_mstdn_id("1", "2"), Ordering::Less);
+        assert_eq!(cmp_mstdn_id("2", "2"), Ordering::Equal);
+        assert_eq!(cmp_mstdn_id("9", "910"), Ordering::Less);
+        assert_eq!(cmp_mstdn_id("1234", "123"), Ordering::Greater);
+        assert_eq!(cmp_mstdn_id("ABC", "ABD"), Ordering::Less);
+
+        let early_id = now_order58();
+        sleep(Duration::from_millis(10));
+        let late_id = now_order58();
+
+        assert_eq!(cmp_mstdn_id(&early_id, &late_id), Ordering::Less);
+        assert_eq!(cmp_mstdn_id(&late_id, &early_id), Ordering::Greater);
+        assert_eq!(cmp_mstdn_id(&early_id, &early_id), Ordering::Equal);
     }
 }
