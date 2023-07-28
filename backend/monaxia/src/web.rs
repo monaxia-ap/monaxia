e5f8e56e7f8ea1b2e9eb2a3f0727459d600d9ac7
Yū Kobayashi <kb10uy@kb10uy.org>
2023-07-29 04:29:42 +0900

    Add trace layer

--- backend/monaxia/src/web.rs
+++ backend/monaxia/src/web.rs
@@ -8,8 +8,10 @@ use axum::{
     routing::{get, post},
     Router,
 };
+use tower_http::trace::TraceLayer;
 
 pub fn construct_router(state_source: state::AppState) -> Router<()> {
+    // routes
     let meta_router = Router::new()
         .route("/host-meta", get(routes::meta::host_meta))
         .route(
@@ -21,14 +23,17 @@ pub fn construct_router(state_source: state::AppState) -> Router<()> {
             get(routes::meta::wellknown_nodeinfo),
         )
         .route("/nodeinfo/2.1", get(routes::meta::nodeinfo));
-
     let users_router = Router::new()
         .route("/:user_id", get(routes::users::actor))
         .route("/:user_id/inbox", post(routes::users::inbox))
         .route("/:user_id/outbox", get(routes::users::outbox));
 
+    // layers
+    let trace_layer = TraceLayer::new_for_http();
+
     Router::new()
         .merge(meta_router)
         .nest("/users", users_router)
         .with_state(state_source)
+        .layer(trace_layer)
 }
