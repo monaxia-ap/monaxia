be641b4c3e3b269d06c17de268c870b9ac387b00
Yū Kobayashi <kb10uy@kb10uy.org>
2023-07-29 05:10:28 +0900

    Add Accept header log

--- backend/monaxia/src/web.rs
+++ backend/monaxia/src/web.rs
@@ -5,10 +5,12 @@ mod routes;
 pub mod state;
 
 use axum::{
+    http::{header::ACCEPT, Request},
     routing::{get, post},
     Router,
 };
-use tower_http::trace::TraceLayer;
+use tower_http::trace::{OnRequest, TraceLayer};
+use tracing::{debug, Span};
 
 pub fn construct_router(state_source: state::AppState) -> Router<()> {
     // routes
@@ -29,11 +31,25 @@ pub fn construct_router(state_source: state::AppState) -> Router<()> {
         .route("/:user_id/outbox", get(routes::users::outbox));
 
     // layers
-    let trace_layer = TraceLayer::new_for_http();
+    let trace_layer = TraceLayer::new_for_http().on_request(OnRequestHandler);
 
     Router::new()
         .merge(meta_router)
         .nest("/users", users_router)
         .with_state(state_source)
         .layer(trace_layer)
+}
+
+#[derive(Debug, Clone)]
+struct OnRequestHandler;
+
+impl<B> OnRequest<B> for OnRequestHandler {
+    fn on_request(&mut self, request: &Request<B>, _: &Span) {
+        let accept = request
+            .headers()
+            .get(ACCEPT)
+            .map(|a| a.to_str().unwrap_or("<binary>"))
+            .unwrap_or_default();
+        debug!("started processing request (Accept: {accept})");
+    }
 }
